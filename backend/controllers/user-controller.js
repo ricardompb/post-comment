d8a67703a7f42aa0036711517715b0b8822ff183
Ricardo Pereira <ricardo.pereira@sisloc.com.br>
2023-05-16 11:38:04 -0300

    manter dados do usuário

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,10 +1,47 @@
 const router = require('express').Router()
 const db = require('../src/db')
-const user = require('../models/user')(db.sequelize, db.DataTypes)
+const User = require('../models/user')(db.sequelize, db.DataTypes)
+const bcrypt = require('bcryptjs')
 
-router.get('/', async (req, res) => {
-    const users = await user.findAll()
+router.get('/', async (req, res) => {    
+    const { login } = req.query
+    const where = login ? { where: { login } } : undefined
+    const users = await User.findAll(where)
+    if (users.length === 0) {
+        return res.json({ message: 'Nenhum usuário encontrado.' })
+    }
     res.json(users)
+})
+
+router.get('/:id', async (req, res) => {
+    const id = parseInt(req.params.id)
+    const user = await User.findByPk(id)
+    if (!user) {
+        return res.json({ message: 'Usuário não encontrado.' })
+    }
+    res.json(user)
+})
+
+router.post('/', async (req, res) => {
+    const salt = bcrypt.genSaltSync(10)
+    const { login, senha } = req.body
+    const user = await User.create({
+        login,
+        senha: bcrypt.hashSync(senha, salt)
+    })
+    return res.json(user)
+})
+
+router.put('/', async (req, res) => {})
+
+router.delete('/:id', async (req, res) => {
+    const user = await User.findByPk(req.params.id)
+    if (!user) {
+        return res.json({ message: 'Usuário não encontrado.' })
+    }
+    const { login } = user.dataValues
+    await user.destroy()
+    res.json({ message: `Usuário '${login}' excluido com sucesso...` })
 })
 
 module.exports = router
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,9 @@
     const db = require('./db')
     await db.connect()    
     const express = require('express')    
-    const app = express()    
+    const app = express() 
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))   
     const userController = require('../controllers/user-controller')    
     app.use('/user/v1', userController)    
     app.listen(process.env.SERVER_PORT, () => {
